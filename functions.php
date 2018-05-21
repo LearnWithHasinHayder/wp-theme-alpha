45a32deb8564e10e0dbc65bb3fc76225ca553563
Hasin Hayder <countdraculla@gmail.com>
2018-05-21 20:45:54 +0600

    sidebar

--- functions.php
+++ functions.php
@@ -4,7 +4,6 @@ function alpha_bootstrapping(){
     load_theme_textdomain("alpha");
     add_theme_support("post-thumbnails");
     add_theme_support("title-tag");
-    register_nav_menu("primary","Top Menu");
 }
 add_action("after_setup_theme","alpha_bootstrapping");
 
@@ -13,3 +12,19 @@ function alpha_assets(){
     wp_enqueue_style("bootstrap","//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 add_action("wp_enqueue_scripts","alpha_assets");
+
+function alpha_sidebar(){
+    register_sidebar(
+        array(
+            'name'          => __( 'Single Post Sidebar', 'alpha' ),
+            'id'            => 'sidebar-1',
+            'description'   => __( 'Right Sidebar', 'alpha' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+
+add_action("widgets_init","alpha_sidebar");
--- single.php
+++ single.php
@@ -1,68 +1,82 @@
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 <?php get_template_part( "hero" ); ?>
-    <div class="posts">
-        <?php
-        while ( have_posts() ) :
-            the_post();
-            ?>
-            <div class="post" <?php post_class(); ?>>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-10 offset-md-1">
-                            <h2 class="post-title text-center">
-                                <?php the_title(); ?>
-                            </h2>
-                            <p class="text-center">
-                                <em><?php the_author(); ?></em><br/>
-                                <?php echo get_the_date(); ?>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-10 offset-md-1">
-                            <p>
-                                <?php
-                                if ( has_post_thumbnail() ) {
-                                    the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
-                                }
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="posts">
+                    <?php
+                    while ( have_posts() ) :
+                        the_post();
+                        ?>
+                        <div class="post" <?php post_class(); ?>>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h2 class="post-title">
+                                            <?php the_title(); ?>
+                                        </h2>
+                                        <p class="">
+                                            <em><?php the_author(); ?></em><br/>
+                                            <?php echo get_the_date(); ?>
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <p>
+                                            <?php
+                                            if ( has_post_thumbnail() ) {
+                                                the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
+                                            }
 
-                                the_content();
+                                            the_content();
 
-                                next_post_link();
-                                echo "<br/>";
-                                previous_post_link();
+                                            next_post_link();
+                                            echo "<br/>";
+                                            previous_post_link();
 
-                                ?>
-                            </p>
-                        </div>
-                        <?php /*if ( comments_open() ): */?><!--
+                                            ?>
+                                        </p>
+                                    </div>
+                                    <?php /*if ( comments_open() ): */?><!--
                             <div class="col-md-10 offset-md-1">
                                 <?php
-/*                                comments_template();
-                                */?>
+                                    /*                                comments_template();
+                                                                    */?>
                             </div>
                         --><?php /*endif; */?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    endwhile;
+                    ?>
+
+                    <div class="container post-pagination">
+                        <div class="row">
+                            <div class="col-md-4"></div>
+                            <div class="col-md-8">
+                                <?php
+                                the_posts_pagination( array(
+                                    "screen_reader_text" => ' ',
+                                    "prev_text"          => "New Posts",
+                                    "next_text"          => "Old Posts"
+                                ) );
+                                ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        <?php
-        endwhile;
-        ?>
-
-        <div class="container post-pagination">
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-8">
-                    <?php
-                    the_posts_pagination( array(
-                        "screen_reader_text" => ' ',
-                        "prev_text"          => "New Posts",
-                        "next_text"          => "Old Posts"
-                    ) );
-                    ?>
-                </div>
+            <div class="col-md-4">
+                <?php
+                if(is_active_sidebar("sidebar-1")){
+                    dynamic_sidebar("sidebar-1");
+                }
+                ?>
             </div>
         </div>
     </div>
+
 <?php get_footer(); ?>
