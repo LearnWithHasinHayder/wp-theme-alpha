a79027729da98cf067ae2d0044a0f809c263076b
Hasin Hayder <countdraculla@gmail.com>
2018-05-23 20:24:22 +0600

    cache busting

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+
+if ( site_url() == "http://demo.lwhh.com" ) {
+    define( "VERSION", time() );
+} else {
+    define( "VERSION", wp_get_theme()->get( "Version" ) );
+}
+
+
 function alpha_bootstrapping() {
     load_theme_textdomain( "alpha" );
     add_theme_support( "post-thumbnails" );
@@ -11,13 +19,16 @@ function alpha_bootstrapping() {
 add_action( "after_setup_theme", "alpha_bootstrapping" );
 
 function alpha_assets() {
-    wp_enqueue_style( "alpha", get_stylesheet_uri() );
+    wp_enqueue_style( "alpha", get_stylesheet_uri(), null, VERSION );
     wp_enqueue_style( "bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" );
     wp_enqueue_style( "featherlight-css", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.css" );
 
     wp_enqueue_script( "featherlight-js", "//cdn.rawgit.com/noelboss/featherlight/1.7.13/release/featherlight.min.js", array( "jquery" ), "0.0.1", true );
 
-    wp_enqueue_script("alpha-main",get_theme_file_uri("/assets/js/main.js"),array("jquery","featherlight-js"),"0.0.1",true);
+    wp_enqueue_script( "alpha-main", get_theme_file_uri( "/assets/js/main.js" ), array(
+        "jquery",
+        "featherlight-js"
+    ), VERSION, true );
 }
 
 add_action( "wp_enqueue_scripts", "alpha_assets" );
