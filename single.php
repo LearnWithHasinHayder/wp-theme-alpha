4b232cfb29897aac6fb5416805a105b4ce3bf67f
Hasin Hayder <countdraculla@gmail.com>
2018-05-29 18:31:55 +0600

    author image and sidebar check

--- single.php
+++ single.php
@@ -1,9 +1,18 @@
+<?php
+$alpha_layout_class = "col-md-8";
+$alpha_text_class = "";
+if ( !is_active_sidebar( "sidebar-1" ) ) {
+    $alpha_layout_class = "col-md-10 offset-md-1";
+    $alpha_text_class = "text-center";
+}
+?>
+
 <?php get_header(); ?>
 <body <?php body_class(); ?>>
 <?php get_template_part( "/template-parts/common/hero" ); ?>
     <div class="container">
         <div class="row">
-            <div class="col-md-8">
+            <div class="<?php echo $alpha_layout_class; ?>">
                 <div class="posts" <?php ?>>
                     <?php
                     while ( have_posts() ) :
@@ -13,10 +22,10 @@
                             <div class="container">
                                 <div class="row">
                                     <div class="col-md-12">
-                                        <h2 class="post-title">
+                                        <h2 class="post-title <?php echo $alpha_text_class; ?>">
                                             <?php the_title(); ?>
                                         </h2>
-                                        <p class="">
+                                        <p class="<?php echo $alpha_text_class; ?>">
                                             <em><?php the_author(); ?></em><br/>
                                             <?php echo get_the_date(); ?>
                                         </p>
@@ -27,8 +36,8 @@
                                         <p>
                                             <?php
                                             if ( has_post_thumbnail() ) {
-                                                $thumbnail_url = get_the_post_thumbnail_url(null,"large");
-                                                printf( '<a class="popup" href="%s" data-featherlight="image">',$thumbnail_url);
+                                                $thumbnail_url = get_the_post_thumbnail_url( null, "large" );
+                                                printf( '<a class="popup" href="%s" data-featherlight="image">', $thumbnail_url );
                                                 the_post_thumbnail( "large", array( "class" => "img-fluid" ) );
                                                 echo '</a>';
                                             }
@@ -40,16 +49,28 @@
                                             ?>
                                         </p>
                                     </div>
-                                    <div class="col-md-10 offset-md-1">
+
+                                    <div class="authorsection">
                                         <div class="row">
-                                            <div class="col-md-4">
+                                            <div class="col-md-2 authorimage">
+                                                <?php
+                                                echo get_avatar( get_the_author_meta( "id" ) );
+                                                ?>
+                                            </div>
+                                            <div class="col-md-10">
+                                                <h4>
+                                                    <?php echo get_the_author_meta( "display_name" ); ?>
+                                                </h4>
+                                                <p>
+                                                    <?php echo get_the_author_meta( "description" ); ?>
+                                                </p>
 
                                             </div>
-                                            <div class="col-md-8"></div>
                                         </div>
                                     </div>
+
                                     <?php if ( comments_open() ): ?>
-                                        <div class="col-md-10 offset-md-1">
+                                        <div class="col-md-12">
                                             <?php
                                             //comments_template();
                                             ?>
@@ -78,13 +99,17 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <?php
-                if ( is_active_sidebar( "sidebar-1" ) ) {
-                    dynamic_sidebar( "sidebar-1" );
-                }
+            <?php
+            if ( is_active_sidebar( "sidebar-1" ) ):
                 ?>
-            </div>
+                <div class="col-md-4">
+                    <?php
+                    if ( is_active_sidebar( "sidebar-1" ) ) {
+                        dynamic_sidebar( "sidebar-1" );
+                    }
+                    ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 
